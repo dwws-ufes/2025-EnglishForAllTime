f76b0bbcc37698b568d52d73f7751c312c101e43
Matheus de Oliveira <matheusdeoliveiraes@gmail.com>
2025-06-10 16:52:46 -0300

    Criada as entidades JPA Course.java e Difficulty.java

---
+++ website/backend/src/main/java/com/backend/domain/Course.java
@@ -0,0 +1,23 @@
+package com.backend.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "courses")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "difficulty_id", nullable = false)
+    private Difficulty difficulty;
+}
